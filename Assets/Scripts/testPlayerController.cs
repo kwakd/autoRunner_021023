3524acafa5ca249e9775d21356a203571eef5f08
kwakd <kwakd.oregonstate.edu>
2022-12-22 15:54:44 -0800

    Player can now detect the ground
    Player is able to tell if grounded.
    Implementing double jump next.

--- Assets/Scripts/testPlayerController.cs
+++ Assets/Scripts/testPlayerController.cs
@@ -24,23 +24,35 @@ public class testPlayerController : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
 
+    public bool isGrounded;
+
+    public LayerMask whatIsGround;
+
     private Rigidbody2D myRigidBody;
+    private Collider2D myCollider;
 
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        isGrounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
         //moves the player
         myRigidBody.velocity = new Vector2(moveSpeed, myRigidBody.velocity.y);
 
         if(Input.GetKeyDown(KeyCode.Space)){
-            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
+            if(isGrounded)
+            {
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
+            }
         }
     }
 
