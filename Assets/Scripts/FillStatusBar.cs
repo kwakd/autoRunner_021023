1a00874d7f781ec8ccaaec6b71acf79ace211cda
kwakd <kwakd.oregonstate.edu>
2022-12-24 16:11:16 -0800

    canvas changes
    - made canvas object
    - made healthbar script and object
    - works correctly to health value for player

---
+++ Assets/Scripts/FillStatusBar.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FillStatusBar : MonoBehaviour
+{
+    public HealthSystem myHealth;
+    public Image fillImage;
+
+    private Slider slider; 
+
+    void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if player RIP
+        if(slider.value <= slider.minValue)
+        {
+            fillImage.enabled = false;
+        }
+        else if(slider.value > slider.minValue && !fillImage.enabled)
+        {
+            fillImage.enabled = true;
+        }
+
+        float fillValue = myHealth.currentHealth / myHealth.maxHealth;
+
+        //if lifebar goes below n/3
+        if(fillValue <= slider.maxValue / 3)
+        {
+            //critical condition
+            fillImage.color = Color.red;
+        }
+        else if(fillValue > slider.maxValue / 3)
+        {
+            //default condition
+            fillImage.color = Color.blue;
+        }
+
+        slider.value = fillValue;
+        
+    }
+}
