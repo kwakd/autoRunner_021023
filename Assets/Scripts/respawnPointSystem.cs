d13dfba6753d022287fad5252fb73d026e7395ec
kwakd <kwakd.oregonstate.edu>
2023-01-10 14:03:59 -0800

    testPlayer2 fab added
    just added testPlayer2 preFab along with looking into respawn system. main objective is to find a way to spawn a stage as the game progresses.

--- Assets/Scripts/respawnPointSystem.cs
+++ Assets/Scripts/respawnPointSystem.cs
@@ -12,12 +12,6 @@ public class respawnPointSystem : MonoBehaviour
         respawnAnimator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.name == "testPlayer2")
